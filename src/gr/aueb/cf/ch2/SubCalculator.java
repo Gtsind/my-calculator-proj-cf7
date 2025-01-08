f33e159bcd49d906614208a6711ad422e4573d3a
Gtsind <tsindarisg@gmail.com>
2025-01-08 19:43:40 +0200

    Initial commit

---
+++ src/gr/aueb/cf/ch2/SubCalculator.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch2;
+
+public class SubCalculator {
+
+    public static void main(String[] args) {
+
+        int num1 = 8;
+        int num2 = 2;
+        int result = 0;
+
+        result = subtract(num1,num2);
+    }
+
+    public static int subtract(int a, int b){
+        return a - b;
+    }
+}
