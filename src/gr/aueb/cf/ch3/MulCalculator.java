f30f02fdf2993f55735256eeb7fcf74df48fae22
Gtsind <tsindarisg@gmail.com>
2025-01-08 20:04:37 +0200

    New feature - mulCalculator

---
+++ src/gr/aueb/cf/ch3/MulCalculator.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch3;
+
+public class MulCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int result = 0;
+
+        result = multiply(num1,num2);
+
+        System.out.println("Multiply : " + result);
+    }
+
+    public static int multiply(int a ,int b) {
+        result = a * b;
+    }
+}
